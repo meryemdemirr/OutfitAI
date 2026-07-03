b345d213ddea9eefaf1f3af73b3182f9c2dd8ca0
Meryem Demir <meryemdmirr0@gmail.com>
2026-07-03 23:53:20 +0300

    Initial Commit

---
+++ OutfitAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OutfitAI
+//
+//  Created by Meryem Demir on 3.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OutfitAI/OutfitAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutfitAIApp.swift
+//  OutfitAI
+//
+//  Created by Meryem Demir on 3.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct OutfitAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
